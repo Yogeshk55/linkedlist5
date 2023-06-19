7585d7ee9063fb99c65db7b0cf19be27e507a94d
Yogeshk55 <136954407+Yogeshk55@users.noreply.github.com>
2023-06-19 06:34:37 -0700

    Add files via upload

---
+++ yogesh.java
@@ -0,0 +1,52 @@
+package linkedlist;
+
+public class ll {
+
+    public static class Node {
+        char data;
+        Node link;
+        Node(char data){
+        this.data = data;
+        }
+        public static int count(Node x){
+            int start = 0;
+            Node temp = x;
+            while(temp!=null){
+            temp = temp.link;
+            start++;
+
+            }
+            return start;
+        }
+        public static void Display(Node x){
+        Node temp = x;
+        while(temp!=null){
+        System.out.print(temp.data);
+        temp = temp.link;
+
+
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        Node x = new Node('y');
+        Node y = new Node('o');
+        Node z = new Node('g');
+        Node g = new Node('e');
+        Node h = new Node('S');
+        Node i = new Node ('h');
+        x.link = y;
+        y.link = z;
+        z.link = g;
+        g.link = h;
+        h.link = i;
+        
+       
+        
+     System.out.println(count(x));
+     Display(x);
+    } 
+
+    }
+}
